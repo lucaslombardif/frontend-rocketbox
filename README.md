4abe6c2c07038fbc78b2746d57c5c2d213b0f48d
lucaslombardif <lucaas-floriano@hotmail.com>
2020-01-21 10:48:03 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,61 @@
+<h1 align="center">
+    <img alt="RocketBox" src="https://github.com/lucaslombardif/frontend-rocketbox/blob/master/src/assets/logo.svg?sanitize=true" />
+    <br>
+    Frontend Rocketbox, um pequeno Clone do Dropbox. <br />
+    ReactJS
+</h1>
+
+<h4 align="center">
+
+</h4>
+
+<p align="center">
+  <a href="#rocket-technologies">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#information_source-how-to-use">Como Usar</a>
+</p>
+
+## :rocket: Tecnologias
+
+Esse projeto foi desenvolvido [durante a semana OmniStack 6.0](https://rocketseat.com.br) com as seguintes tecnologias:
+
+-  [Node.js][nodejs]
+-  [Socket.io](https://socket.io/)
+-  [ReactJS](https://reactjs.org/)
+-  [React Router](https://github.com/ReactTraining/react-router)
+-  [styled-components](https://www.styled-components.com/)
+-  [Dropzone](https://github.com/react-dropzone/react-dropzone)
+-  [React-icons](https://www.npmjs.com/package/react-icons)
+-  [Date-fns](https://date-fns.org/)
+-  [axios](https://github.com/axios/axios)
+-  [VS Code][vc]
+
+## :information_source: Como usar
+
+Para clonar e rodar essa aplicação você precisa do [Git](https://git-scm.com), [ReactJS][reactjs] e [Yarn][yarn] instalados em seu computador. 
+
+Para rodar via linha de comando utilize: 
+
+```bash
+# Clone o Repositório
+$  git clone https://github.com/lucaslombardif/frontend-rocketbox.git
+# Va até o diretório
+$ cd frontend-rocketbox
+# Instale as Dependências
+$ yarn install
+# Inicie a aplicação 
+$ yarn start
+```
+Ou vá aqui [aqui](https://lucaslombardif.codes).
+
+[nodejs]: https://nodejs.org/en/
+[reactjs]: https://pt-br.reactjs.org/
+[yarn]: https://yarnpkg.com/
+[vc]: https://code.visualstudio.com/
+
+---------
+
+## Link do Backend: 
+
+ - https://omnistack-six.herokuapp.com
+ - https://github.com/lucaslombardif/backend-rocketbox
+ 
---
+++ src/pages/Box/index.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { MdInsertDriveFile } from 'react-icons/md'
+import logo from '../../assets/logo.svg'
+import './styles.css';
+import api from '../../services/api'
+import { distanceInWords } from 'date-fns'
+import pt from 'date-fns/locale/pt'
+import Dropzone from 'react-dropzone'
+import socket from 'socket.io-client'
+export default class Box extends Component {
+
+    state = { box: {} }
+
+    handleUpload = (files) => {
+        files.forEach((file => {
+            const data = new FormData();
+            const box = this.props.match.params.id;
+            data.append('file', file)
+            api.post(`boxes/${box}/files`, data)
+        }))
+    }
+
+    async componentDidMount() {
+        this.subscribeToNewFiles();
+        const box = this.props.match.params.id;
+        const response = await api.get(`boxes/${box}`)
+        this.setState({ box: response.data });
+    }
+
+    subscribeToNewFiles = () => {
+        const box = this.props.match.params.id;
+        const io = socket('https://omnistack-six.herokuapp.com')
+        io.emit('connectRoom', box)
+        io.on('file', data => {
+            this.setState({ box: { ... this.state.box, files: [data, ... this.state.box.files] } })
+        })
+    }
+
+    render() {
+        return (
+            <div id="box-container">
+                <header>
+                    <img src={logo} alt='' />
+                    <h1>{this.state.box.title}</h1>
+                </header>
+
+
+                <Dropzone onDropAccepted={this.handleUpload}>
+
+                    {({ getRootProps, getInputProps }) => (
+                        <div className="upload" {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <p>Arraste arquivos ou clique aqui</p>
+                        </div>
+                    )}
+
+                </Dropzone>
+
+
+                <ul>
+                    {this.state.box.files && this.state.box.files.map(file => (
+                        <li key={file._id}>
+                            <a class='fileInfo' href={file.url} target='_blank'>
+                                <MdInsertDriveFile size={24} color='#A5Cfff' />
+                                <strong>{file.title}</strong>
+                            </a>
+                            <span>Há {" "} {distanceInWords(file.createdAt, new Date(), { locale: pt })}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+
+            </div>
+        );
+    }
+}
